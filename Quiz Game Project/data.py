9c1de3e4ad68e52ee476796a48216da4a9eca750
happy09123 <108203368+happy09123@users.noreply.github.com>
2023-07-05 12:41:41 +0800

    Add files via upload

---
+++ Quiz Game Project/data.py
@@ -0,0 +1,49 @@
+question_data = [
+    {
+        "text": "Linus Torvalds created Linux and Git.",
+        "answer": "True",
+    },
+    {
+        "text": "The programming language 'Python' is based off a modified version of 'JavaScript'.",
+        "answer": "False",
+    },
+    {
+        "text": "Pointers were not used in the original C programming language; they were added later on in C++.",
+        "answer": "False",
+    },
+    {
+        "text": "RAM stands for Random Access Memory.", "answer": "True",
+    },
+    {
+        "text": "Ada Lovelace is often considered the first computer programmer.",
+        "answer": "True",
+    },
+    {
+        "text": "'HTML' stands for Hypertext Markup Language.",
+        "answer": "True",
+    },
+    {
+        "text": "In most programming languages, the operator ++ is equivalent to the statement '+= 1'.",
+        "answer": "True",
+    },
+    {
+        "text": "The Windows 7 operating system has six main editions.",
+        "answer": "True",
+    },
+    {
+        "text": "The Windows ME operating system was released in the year 2000.",
+        "answer": "True",
+    },
+    {
+        "text": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+        "answer": "False",
+    },
+    {
+        "text": "Linux was first created as an alternative to Windows XP.",
+        "answer": "False",
+    },
+    {
+        "text": "The Python programming language gets its name from the British comedy group 'Monty Python.'",
+        "answer": "True",
+    }
+]
